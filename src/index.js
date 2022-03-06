b8f337cbe3746249b3c14efc38374806853337de
Francisco <montejof1998@gmail.com>
2022-03-05 19:11:38 -0800

    Mobile design ready

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import "../src/styles/global.scss";
+import App from "./components/App.jsx";
+
+ReactDOM.render(<App />, document.getElementById("root"));
