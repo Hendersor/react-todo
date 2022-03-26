ea1c3b8d18249eea2298e742cb4a5758bd32b283
Francisco <montejof1998@gmail.com>
2022-03-26 12:37:32 -0700

    Notasks component was created

---
+++ src/components/Notasks.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./../styles/Notasks.scss";
+
+const Notasks = () => {
+  return (
+    <section className="infoContainer">
+      <h1>No tasks</h1>
+    </section>
+  );
+};
+
+export { Notasks };
---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+//The setup for the local storage using a custom hook
+function useLocalStorage(itemName, initialValue) {
+  const localStorageItem = localStorage.getItem(itemName);
+  let parsedItem;
+
+  if (!localStorageItem) {
+    localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parsedItem = initialValue;
+  } else {
+    parsedItem = JSON.parse(localStorageItem);
+  }
+
+  const [item, setItem] = React.useState(parsedItem);
+  const saveItem = (newItem) => {
+    const stringifiedItem = JSON.stringify(newItem);
+    localStorage.setItem(itemName, stringifiedItem);
+    setItem(newItem);
+  };
+
+  return [item, saveItem];
+}
+
+export { useLocalStorage };
